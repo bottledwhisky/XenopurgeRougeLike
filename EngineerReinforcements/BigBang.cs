d92772f9756268c22115ef2e3417c5de3e0e2bfd
bottledwhisky <215269529+bottledwhisky@users.noreply.github.com>
2026-02-04 00:18:30 +0100

    Bigbang fix

--- EngineerReinforcements/BigBang.cs
+++ EngineerReinforcements/BigBang.cs
@@ -1,10 +1,13 @@
 using HarmonyLib;
 using SpaceCommander;
+using SpaceCommander.Achievements;
 using SpaceCommander.ActionCards;
 using SpaceCommander.Area;
+using SpaceCommander.Weapons;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using Traptics.EventsSystem;
 using static XenopurgeRougeLike.ModLocalization;
 
 namespace XenopurgeRougeLike.EngineerReinforcements
@@ -58,7 +61,7 @@ namespace XenopurgeRougeLike.EngineerReinforcements
             if (BigBang.Instance.IsActive)
             {
                 // Get GridManager from the BattleManager singleton
-                var gridManager = GetGridManager();
+                var gridManager = GameManager.Instance.GridManager;
                 if (gridManager != null)
                 {
                     // Get all adjacent tiles (passable neighbors)
@@ -71,39 +74,6 @@ namespace XenopurgeRougeLike.EngineerReinforcements
             }
 
             return units;
-        }
-
-        /// <summary>
-        /// Gets the GridManager from the BattleManager
-        /// </summary>
-        private static GridManager GetGridManager()
-        {
-            try
-            {
-                // Try to get BattleManager instance
-                var battleManagerType = AccessTools.TypeByName("SpaceCommander.BattleManagement.BattleManager");
-                if (battleManagerType == null)
-                    return null;
-
-                var instanceProperty = AccessTools.Property(battleManagerType, "Instance");
-                if (instanceProperty == null)
-                    return null;
-
-                var battleManager = instanceProperty.GetValue(null);
-                if (battleManager == null)
-                    return null;
-
-                // Get GridManager from BattleManager
-                var gridManagerField = AccessTools.Field(battleManagerType, "_gridManager");
-                if (gridManagerField == null)
-                    return null;
-
-                return gridManagerField.GetValue(battleManager) as GridManager;
-            }
-            catch
-            {
-                return null;
-            }
         }
 
         /// <summary>
@@ -164,22 +134,15 @@ namespace XenopurgeRougeLike.EngineerReinforcements
             {
                 try
                 {
-                    var achievementsManagerType = AccessTools.TypeByName("SpaceCommander.Achievements.AchievementsManager");
-                    if (achievementsManagerType != null)
+                    var achievementsManager = AchievementsManager.Instance;
+
+                    if (achievementsManager != null)
                     {
-                        var instanceProperty = AccessTools.Property(achievementsManagerType, "Instance");
-                        var achievementsManager = instanceProperty?.GetValue(null);
+                        achievementsManager.KilledUnitsWithOneGrenade(killedUnits.Count, __instance.Info.Id);
 
-                        if (achievementsManager != null)
+                        if (killedUnits.Any(x => x.Team == Enumerations.Team.Player))
                         {
-                            var killedMethod = AccessTools.Method(achievementsManagerType, "KilledUnitsWithOneGrenade");
-                            killedMethod?.Invoke(achievementsManager, new object[] { killedUnits.Count, __instance.Info.Id });
-
-                            if (killedUnits.Any(x => x.Team == Enumerations.Team.Player))
-                            {
-                                var friendlyFireMethod = AccessTools.Method(achievementsManagerType, "FriendlyFire");
-                                friendlyFireMethod?.Invoke(achievementsManager, null);
-                            }
+                            achievementsManager.FriendlyFire();
                         }
                     }
                 }
@@ -190,22 +153,8 @@ namespace XenopurgeRougeLike.EngineerReinforcements
             }
 
             // Trigger explosion visual effect
-            try
-            {
-                var eventBusType = AccessTools.TypeByName("Traptics.EventsSystem.EventBus");
-                var triggerExplosionEventType = AccessTools.TypeByName("Traptics.EventsSystem.TriggerExplosionEvent");
-
-                if (eventBusType != null && triggerExplosionEventType != null)
-                {
-                    var publishMethod = AccessTools.Method(eventBusType, "Publish");
-                    var explosionEvent = System.Activator.CreateInstance(triggerExplosionEventType, centerTile);
-                    publishMethod?.Invoke(null, new object[] { explosionEvent });
-                }
-            }
-            catch
-            {
-                // Ignore visual effect errors
-            }
+            var explosionEvent = new TriggerExplosionEvent(centerTile);
+            EventBus.Publish(explosionEvent);
 
             return false; // Skip original method
         }
@@ -217,34 +166,107 @@ namespace XenopurgeRougeLike.EngineerReinforcements
     [HarmonyPatch(typeof(ChangeStatArea_Card), "ApplyCommand")]
     public static class BigBang_FlashbangArea_Patch
     {
-        public static bool Prefix(ChangeStatArea_Card __instance, BattleUnit unit)
+        // Field accessors for ChangeStatArea_Card private fields
+        private static readonly AccessTools.FieldRef<ChangeStatArea_Card, List<StatChange>> _statChangesRef =
+            AccessTools.FieldRefAccess<ChangeStatArea_Card, List<StatChange>>("_statChanges");
+
+        public static void Prefix(ChangeStatArea_Card __instance, BattleUnit unit, ref List<BattleUnit> __state)
         {
             // Only override if Big Bang is active and this is a relevant card
             if (!BigBangHelpers.ShouldExtendRange(__instance))
-                return true; // Run original method
+            {
+                __state = null;
+                return;
+            }
 
-            // Custom implementation with extended range
+            // Get all units that should be affected (center + neighbors)
             Tile centerTile = unit.MovementManager.CurrentTile;
             List<BattleUnit> affectedUnits = BigBangHelpers.GetUnitsInExtendedArea(centerTile);
 
-            // Get the stat change parameters using reflection
-            var statToChangeField = AccessTools.Field(typeof(ChangeStatArea_Card), "_statToChange");
-            var changeValueField = AccessTools.Field(typeof(ChangeStatArea_Card), "_changeValue");
-            var durationField = AccessTools.Field(typeof(ChangeStatArea_Card), "_duration");
-            var addStatChangeMethod = AccessTools.Method(typeof(ChangeStatArea_Card), "AddStatChange");
+            // Get the stat changes list
+            List<StatChange> statChanges = _statChangesRef(__instance);
 
-            var statToChange = (Enumerations.UnitStats)statToChangeField.GetValue(__instance);
-            float changeValue = (float)changeValueField.GetValue(__instance);
-            float duration = (float)durationField.GetValue(__instance);
+            // The original method will handle the center tile
+            // We need to apply effects to ONLY the adjacent tiles (not center)
+            var neighborsOnly = affectedUnits.Except(centerTile.CurrentStateOfTile.UnitsOnTile).ToList();
 
-            // Apply stat change to all affected units
-            foreach (BattleUnit affectedUnit in affectedUnits)
+            // Store neighbor units for postfix to track them for removal
+            __state = neighborsOnly;
+
+            // Apply stat changes to neighbor units immediately
+            foreach (BattleUnit neighborUnit in neighborsOnly)
             {
-                // Call the private AddStatChange method
-                addStatChangeMethod?.Invoke(__instance, new object[] { affectedUnit, statToChange, changeValue, duration });
-            }
+                foreach (StatChange statChange in statChanges)
+                {
+                    // Skip if unit ignores AOE damage and this is a debuff
+                    if (!neighborUnit.IsIgnoringAOEDamage || !(statChange.ChangeValueOfStat < 0f))
+                    {
+                        // Generate a GUID for this stat change so it can be reversed later
+                        string guid = System.Guid.NewGuid().ToString();
 
-            return false; // Skip original method
+                        // We'll track this in our own system since the card only tracks center tile units
+                        BigBang_StatChangeTracker.TrackStatChange(__instance, neighborUnit, guid);
+
+                        neighborUnit.ChangeStat(statChange.StatToChange, statChange.ChangeValueOfStat, guid);
+                    }
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Tracks stat changes applied to neighbor units for later reversal
+    /// </summary>
+    public static class BigBang_StatChangeTracker
+    {
+        private static readonly Dictionary<ChangeStatArea_Card, List<(BattleUnit unit, string guid)>> _trackedChanges = new();
+
+        public static void TrackStatChange(ChangeStatArea_Card card, BattleUnit unit, string guid)
+        {
+            if (!_trackedChanges.ContainsKey(card))
+            {
+                _trackedChanges[card] = new List<(BattleUnit, string)>();
+            }
+            _trackedChanges[card].Add((unit, guid));
+        }
+
+        public static void RemoveTrackedChanges(ChangeStatArea_Card card)
+        {
+            if (_trackedChanges.TryGetValue(card, out var changes))
+            {
+                foreach (var (unit, guid) in changes)
+                {
+                    if (unit != null && unit.IsAlive)
+                    {
+                        unit.ReverseChangeOfStat(guid);
+                    }
+                }
+                _trackedChanges.Remove(card);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Patch OnTimeUpdate to also remove our tracked stat changes when duration expires
+    /// </summary>
+    [HarmonyPatch(typeof(ChangeStatArea_Card), "OnTimeUpdate")]
+    public static class BigBang_FlashbangCleanup_Patch
+    {
+        private static readonly AccessTools.FieldRef<ChangeStatArea_Card, float> _remainingTimeRef =
+            AccessTools.FieldRefAccess<ChangeStatArea_Card, float>("_remainingTime");
+
+        public static void Postfix(ChangeStatArea_Card __instance, float time)
+        {
+            if (!BigBang.Instance.IsActive)
+                return;
+
+            // Check if the duration has expired
+            float remainingTime = _remainingTimeRef(__instance);
+            if (remainingTime <= 0f)
+            {
+                // Remove our tracked stat changes for neighbor units
+                BigBang_StatChangeTracker.RemoveTrackedChanges(__instance);
+            }
         }
     }
 }
